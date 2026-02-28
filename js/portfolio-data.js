3c6fa7eded77a93ce4e29c0519d5e96dc20f5440
shivikatiwari9 <tiwarishivika9@gmail.com>
2026-02-28 01:02:04 +0000

    Refactor image paths and add new editorial images
    Updated image source and thumbnail paths for Vogue HK and Appiok shoots. Added new images for various editorial and fashion categories.

--- js/portfolio-data.js
+++ js/portfolio-data.js
@@ -19,21 +19,405 @@ const portfolioData = {
         // Vogue HK Shoot (4 images)
         {
             id: 'ed-1',
-            src: 'images/editorial/Vogue%20HK1.png',
-            thumb: 'images/editorial/Vogue%20HK1.png',
+            src: 'images/editorial/Vogue HK1.png',
+            thumb: 'images/editorial/Vogue HK1.png',
             alt: 'Vogue Hong Kong editorial shoot',
             group: 'vogue-hk'
         },
         {
             id: 'ed-2',
-            src: 'images/editorial/Vogue%20HK2.png',
-            thumb: 'images/editorial/Vogue%20HK2.png',
+            src: 'images/editorial/Vogue HK2.png',
+            thumb: 'images/editorial/Vogue HK2.png',
             alt: 'Vogue Hong Kong editorial shoot',
             group: 'vogue-hk'
         },
         {
             id: 'ed-3',
-            src: 'images/editorial/Vogue%20HK3.png',
+            src: 'images/editorial/Vogue HK3.png',
+            thumb: 'images/editorial/Vogue HK3.png',
+            alt: 'Vogue Hong Kong editorial shoot',
+            group: 'vogue-hk'
+        },
+        {
+            id: 'ed-4',
+            src: 'images/editorial/Vogue HK4.png',
+            thumb: 'images/editorial/Vogue HK4.png',
+            alt: 'Vogue Hong Kong editorial shoot',
+            group: 'vogue-hk'
+        },
+        // Appiok Shoot (4 images)
+        {
+            id: 'ed-5',
+            src: 'images/editorial/Appiok27668 final no bg color.jpg',
+            thumb: 'images/editorial/Appiok27668 final no bg color.jpg',
+            alt: 'Appiok editorial shoot',
+            group: 'appiok-shoot'
+        },
+        {
+            id: 'ed-6',
+            src: 'images/editorial/Appiok27728 final.jpg',
+            thumb: 'images/editorial/Appiok27728 final.jpg',
+            alt: 'Appiok editorial shoot',
+            group: 'appiok-shoot'
+        },
+        {
+            id: 'ed-7',
+            src: 'images/editorial/Appiok27825 final more texture.jpg',
+            thumb: 'images/editorial/Appiok27825 final more texture.jpg',
+            alt: 'Appiok editorial shoot',
+            group: 'appiok-shoot'
+        },
+        {
+            id: 'ed-8',
+            src: 'images/editorial/Appiok27847 final.jpg',
+            thumb: 'images/editorial/Appiok27847 final.jpg',
+            alt: 'Appiok editorial shoot',
+            group: 'appiok-shoot'
+        },
+        // 26th Feb Magazine Shoot (8 images)
+        {
+            id: 'ed-9',
+            src: 'images/editorial/26th Feb magazine6190.jpg',
+            thumb: 'images/editorial/26th Feb magazine6190.jpg',
+            alt: 'Magazine editorial shoot',
+            group: 'feb-magazine'
+        },
+        {
+            id: 'ed-10',
+            src: 'images/editorial/26th Feb magazine6224.jpg',
+            thumb: 'images/editorial/26th Feb magazine6224.jpg',
+            alt: 'Magazine editorial shoot',
+            group: 'feb-magazine'
+        },
+        {
+            id: 'ed-11',
+            src: 'images/editorial/26th Feb magazine6249.jpg',
+            thumb: 'images/editorial/26th Feb magazine6249.jpg',
+            alt: 'Magazine editorial shoot',
+            group: 'feb-magazine'
+        },
+        {
+            id: 'ed-12',
+            src: 'images/editorial/26th Feb magazine6472.jpg',
+            thumb: 'images/editorial/26th Feb magazine6472.jpg',
+            alt: 'Magazine editorial shoot',
+            group: 'feb-magazine'
+        },
+        {
+            id: 'ed-13',
+            src: 'images/editorial/26th Feb magazine6530.jpg',
+            thumb: 'images/editorial/26th Feb magazine6530.jpg',
+            alt: 'Magazine editorial shoot',
+            group: 'feb-magazine'
+        },
+        {
+            id: 'ed-14',
+            src: 'images/editorial/26th Feb magazine6540.jpg',
+            thumb: 'images/editorial/26th Feb magazine6540.jpg',
+            alt: 'Magazine editorial shoot',
+            group: 'feb-magazine'
+        },
+        {
+            id: 'ed-15',
+            src: 'images/editorial/26th Feb magazine6694.jpg',
+            thumb: 'images/editorial/26th Feb magazine6694.jpg',
+            alt: 'Magazine editorial shoot',
+            group: 'feb-magazine'
+        },
+        {
+            id: 'ed-16',
+            src: 'images/editorial/26th Feb magazine6707.jpg',
+            thumb: 'images/editorial/26th Feb magazine6707.jpg',
+            alt: 'Magazine editorial shoot',
+            group: 'feb-magazine'
+        },
+        // IMG Series (3 images)
+        {
+            id: 'ed-17',
+            src: 'images/editorial/IMG_6317.JPG',
+            thumb: 'images/editorial/IMG_6317.JPG',
+            alt: 'Editorial shoot',
+            group: 'img-series'
+        },
+        {
+            id: 'ed-18',
+            src: 'images/editorial/IMG_6318.JPG',
+            thumb: 'images/editorial/IMG_6318.JPG',
+            alt: 'Editorial shoot',
+            group: 'img-series'
+        },
+        {
+            id: 'ed-19',
+            src: 'images/editorial/IMG_6320.JPG',
+            thumb: 'images/editorial/IMG_6320.JPG',
+            alt: 'Editorial shoot',
+            group: 'img-series'
+        },
+        {
+            id: 'ed-20',
+            src: 'images/editorial/IMG_2467.JPG',
+            thumb: 'images/editorial/IMG_2467.JPG',
+            alt: 'Editorial shoot',
+            group: 'editorial-misc'
+        },
+        // 12 Feb Grace Shoot (3 images)
+        {
+            id: 'ed-21',
+            src: 'images/editorial/12 feb Grace3816.jpg',
+            thumb: 'images/editorial/12 feb Grace3816.jpg',
+            alt: 'Grace editorial shoot',
+            group: 'grace-shoot'
+        },
+        {
+            id: 'ed-22',
+            src: 'images/editorial/12 feb Grace3822.jpg',
+            thumb: 'images/editorial/12 feb Grace3822.jpg',
+            alt: 'Grace editorial shoot',
+            group: 'grace-shoot'
+        },
+        {
+            id: 'ed-23',
+            src: 'images/editorial/12 feb.jpg',
+            thumb: 'images/editorial/12 feb.jpg',
+            alt: 'Grace editorial shoot',
+            group: 'grace-shoot'
+        },
+        // Iulia Test (1 image)
+        {
+            id: 'ed-24',
+            src: 'images/editorial/Iuliatest.jpg',
+            thumb: 'images/editorial/Iuliatest.jpg',
+            alt: 'Iulia editorial test shoot',
+            group: 'iulia-test'
+        }
+    ],
+
+    // Fashion Category
+    fashion: [
+        // 060A Series (3 images)
+        {
+            id: 'fa-1',
+            src: 'images/fashion/060A8079.jpg',
+            thumb: 'images/fashion/060A8079.jpg',
+            alt: 'Fashion shoot',
+            group: '060a-series'
+        },
+        {
+            id: 'fa-2',
+            src: 'images/fashion/060A8089.jpg',
+            thumb: 'images/fashion/060A8089.jpg',
+            alt: 'Fashion shoot',
+            group: '060a-series'
+        },
+        {
+            id: 'fa-3',
+            src: 'images/fashion/060A8147.jpg',
+            thumb: 'images/fashion/060A8147.jpg',
+            alt: 'Fashion shoot',
+            group: '060a-series'
+        },
+        // DSC Series (2 images)
+        {
+            id: 'fa-4',
+            src: 'images/fashion/DSC04011 copy.jpg',
+            thumb: 'images/fashion/DSC04011 copy.jpg',
+            alt: 'Fashion shoot',
+            group: 'dsc-series'
+        },
+        {
+            id: 'fa-5',
+            src: 'images/fashion/DSC04320 copy (1).jpg',
+            thumb: 'images/fashion/DSC04320 copy (1).jpg',
+            alt: 'Fashion shoot',
+            group: 'dsc-series'
+        },
+        // Jennifer Cole / Linden Staub by Samuel Geals (2 images)
+        {
+            id: 'fa-6',
+            src: 'images/fashion/Jennifer_Cole_Linden_Staub_by_Samuel_Geals_Photography_3.jpg',
+            thumb: 'images/fashion/Jennifer_Cole_Linden_Staub_by_Samuel_Geals_Photography_3.jpg',
+            alt: 'Jennifer Cole - Linden Staub by Samuel Geals Photography',
+            group: 'jennifer-cole-shoot'
+        },
+        {
+            id: 'fa-7',
+            src: 'images/fashion/Jennifer_Cole_Linden_Staub_by_Samuel_Geals_Photography_9.jpg',
+            thumb: 'images/fashion/Jennifer_Cole_Linden_Staub_by_Samuel_Geals_Photography_9.jpg',
+            alt: 'Jennifer Cole - Linden Staub by Samuel Geals Photography',
+            group: 'jennifer-cole-shoot'
+        },
+        // Zhou / Linden Staub by Samuel Geals (1 image)
+        {
+            id: 'fa-8',
+            src: 'images/fashion/Zhou_Linden_Staub_by_Samuel_Geals_Photography_5.jpg',
+            thumb: 'images/fashion/Zhou_Linden_Staub_by_Samuel_Geals_Photography_5.jpg',
+            alt: 'Zhou - Linden Staub by Samuel Geals Photography',
+            group: 'zhou-shoot'
+        },
+        // Other Fashion Images
+        {
+            id: 'fa-9',
+            src: 'images/fashion/IMG-20250629-WA0004.jpg',
+            thumb: 'images/fashion/IMG-20250629-WA0004.jpg',
+            alt: 'Fashion shoot',
+            group: 'fashion-misc-1'
+        },
+        {
+            id: 'fa-10',
+            src: 'images/fashion/IMG_1955.JPG',
+            thumb: 'images/fashion/IMG_1955.JPG',
+            alt: 'Fashion shoot',
+            group: 'fashion-misc-2'
+        },
+        {
+            id: 'fa-11',
+            src: 'images/fashion/IMG_1979.JPG',
+            thumb: 'images/fashion/IMG_1979.JPG',
+            alt: 'Fashion shoot',
+            group: 'fashion-misc-2'
+        },
+        {
+            id: 'fa-12',
+            src: 'images/fashion/9556A787-AB00-4615-9F90-CF7878BF1D1C.jpg',
+            thumb: 'images/fashion/9556A787-AB00-4615-9F90-CF7878BF1D1C.jpg',
+            alt: 'Fashion shoot',
+            group: 'fashion-misc-3'
+        }
+    ],
+
+    // Beauty Category
+    beauty: [
+        // Appiok Beauty (2 images)
+        {
+            id: 'be-1',
+            src: 'images/beauty/Appiok27563 retouched1.jpg',
+            thumb: 'images/beauty/Appiok27563 retouched1.jpg',
+            alt: 'Appiok beauty shoot',
+            group: 'appiok-beauty'
+        },
+        {
+            id: 'be-2',
+            src: 'images/beauty/Appiok27603 final.jpg',
+            thumb: 'images/beauty/Appiok27603 final.jpg',
+            alt: 'Appiok beauty shoot',
+            group: 'appiok-beauty'
+        },
+        // Yasmin Series (2 images)
+        {
+            id: 'be-3',
+            src: 'images/beauty/Yasmin0184finalweb.jpg',
+            thumb: 'images/beauty/Yasmin0184finalweb.jpg',
+            alt: 'Yasmin beauty shoot',
+            group: 'yasmin-beauty'
+        },
+        {
+            id: 'be-4',
+            src: 'images/beauty/Yasmin1617finalweb.jpg',
+            thumb: 'images/beauty/Yasmin1617finalweb.jpg',
+            alt: 'Yasmin beauty shoot',
+            group: 'yasmin-beauty'
+        },
+        // 231032 Series (2 images)
+        {
+            id: 'be-5',
+            src: 'images/beauty/231032106-1.jpg',
+            thumb: 'images/beauty/231032106-1.jpg',
+            alt: 'Beauty shoot',
+            group: '231032-series'
+        },
+        {
+            id: 'be-6',
+            src: 'images/beauty/231032180-1.jpg',
+            thumb: 'images/beauty/231032180-1.jpg',
+            alt: 'Beauty shoot',
+            group: '231032-series'
+        },
+        // IMG Beauty Series
+        {
+            id: 'be-7',
+            src: 'images/beauty/IMG_4416.JPG',
+            thumb: 'images/beauty/IMG_4416.JPG',
+            alt: 'Beauty makeup look',
+            group: 'beauty-img-1'
+        },
+        {
+            id: 'be-8',
+            src: 'images/beauty/IMG_4688.JPG',
+            thumb: 'images/beauty/IMG_4688.JPG',
+            alt: 'Beauty makeup look',
+            group: 'beauty-img-1'
+        },
+        {
+            id: 'be-9',
+            src: 'images/beauty/IMG_4693.JPG',
+            thumb: 'images/beauty/IMG_4693.JPG',
+            alt: 'Beauty makeup look',
+            group: 'beauty-img-2'
+        },
+        {
+            id: 'be-10',
+            src: 'images/beauty/IMG_4694.JPG',
+            thumb: 'images/beauty/IMG_4694.JPG',
+            alt: 'Beauty makeup look',
+            group: 'beauty-img-2'
+        },
+        {
+            id: 'be-11',
+            src: 'images/beauty/IMG_5098.JPG',
+            thumb: 'images/beauty/IMG_5098.JPG',
+            alt: 'Beauty makeup look',
+            group: 'beauty-img-3'
+        },
+        {
+            id: 'be-12',
+            src: 'images/beauty/IMG_5126.JPG',
+            thumb: 'images/beauty/IMG_5126.JPG',
+            alt: 'Beauty makeup look',
+            group: 'beauty-img-3'
+        },
+        {
+            id: 'be-13',
+            src: 'images/beauty/IMG_5127.JPG',
+            thumb: 'images/beauty/IMG_5127.JPG',
+            alt: 'Beauty makeup look',
+            group: 'beauty-img-3'
+        }
+    ],
+
+    // Glam Category
+    glam: [
+        {
+            id: 'gl-1',
+            src: 'images/glam/lizaKoshy.jpg',
+            thumb: 'images/glam/lizaKoshy.jpg',
+            alt: 'Liza Koshy glam makeup',
+            group: 'liza-koshy'
+        },
+        {
+            id: 'gl-2',
+            src: 'images/glam/IMG_2702.JPG',
+            thumb: 'images/glam/IMG_2702.JPG',
+            alt: 'Glam makeup look',
+            group: 'glam-look-1'
+        }
+    ],
+
+    // Bridal Category - Coming Soon
+    bridal: []
+};
+
+// Special message for empty categories
+const categoryMessages = {
+    bridal: {
+        title: 'Coming Soon',
+        message: 'Bridal portfolio is currently being updated. Check back soon for stunning bridal looks!'
+    }
+};
+
+// Export for use in main.js (for module-based systems)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = portfolioData;
+}
             thumb: 'images/editorial/Vogue%20HK3.png',
             alt: 'Vogue Hong Kong editorial shoot',
             group: 'vogue-hk'
